26ee69dd45f6d78fc69bac337b493ed1e6b5d208
Mohamedj23 <mohamedj239@gmail.com>
2019-02-15 08:14:51 +0200

    Revert "testing commiting"
    This reverts commit 766a38cfa14e03375eac46c1bb6497a0b02bf033.

--- MyChatPOM/MyServer/src/main/java/com/mycompany/myserver/controller/MyChatServerController.java
+++ MyChatPOM/MyServer/src/main/java/com/mycompany/myserver/controller/MyChatServerController.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-// ha a l klam ya jemy ?
 package mychatserver.controller;
 
 import commonservice.User;
