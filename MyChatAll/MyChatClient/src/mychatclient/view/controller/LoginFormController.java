70a2b1ac1f93d0d67a7058a447d8ed594bca353e
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2019-02-16 19:01:23 +0200

    connecting the registeration back
    connecting the registeration back with it's button after sahar changes

--- MyChatAll/MyChatClient/src/mychatclient/view/controller/LoginFormController.java
+++ MyChatAll/MyChatClient/src/mychatclient/view/controller/LoginFormController.java
@@ -5,13 +5,22 @@
  */
 package mychatclient.view.controller;
 
+import commonservice.User;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import mychatclient.controller.MyChatClient;
 
 /**
  * FXML Controller class
@@ -19,6 +28,7 @@ import javafx.scene.control.TextField;
  * @author HP
  */
 public class LoginFormController implements Initializable {
+
     @FXML
     private TextField phoneNumberTF;
     @FXML
@@ -32,14 +42,29 @@ public class LoginFormController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void handleSigninButton(ActionEvent event) {
     }
+    User user = null;
+    MyChatClient controller;
+    Stage stage;
 
     @FXML
-    private void handleSignupButton(ActionEvent event) {
+    public void handleSignupButton(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mychatclient/view/view/RegisterForm.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage = (Stage) signUpButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ex) {
+            Logger.getLogger(LoginFormController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
